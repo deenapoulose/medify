f2b4accdaf5d8f6de2e27083c6014dd6d32b839e
Deena-toobler <deena.poulose@zuseapps.co.uk>
2025-04-26 20:47:43 +0530

    test2

--- medify/src/pages/landingPage.js
+++ medify/src/pages/landingPage.js
@@ -1,24 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function LandingPage() {
   const [states, setStates] = useState([]);
   const [cities, setCities] = useState([]);
-  const [selectedState, setSelectedState] = useState('');
-  const [selectedCity, setSelectedCity] = useState('');
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
   const navigate = useNavigate();
- console.log('Landing page')
+  console.log("Landing page");
   useEffect(() => {
-    axios.get('https://meddata-backend.onrender.com/states')
-      .then(res => setStates(res.data))
+    axios
+      .get("https://meddata-backend.onrender.com/states")
+      .then((res) => setStates(res.data))
       .catch(console.error);
   }, []);
 
   useEffect(() => {
     if (selectedState) {
-      axios.get(`https://meddata-backend.onrender.com/cities/${selectedState}`)
-        .then(res => setCities(res.data))
+      axios
+        .get(`https://meddata-backend.onrender.com/cities/${selectedState}`)
+        .then((res) => setCities(res.data))
         .catch(console.error);
     }
   }, [selectedState]);
@@ -30,21 +32,31 @@ function LandingPage() {
   return (
     <div>
       <h1>Find Medical Centers</h1>
-      <div id="state">
-        <select onChange={e => setSelectedState(e.target.value)}>
+      <div>
+        <select id="state" onChange={(e) => setSelectedState(e.target.value)}>
           <option value="">Select State</option>
-          {states.map((s, i) => <option key={i} value={s}>{s}</option>)}
+          {states.map((s, i) => (
+            <option key={i} value={s}>
+              {s}
+            </option>
+          ))}
         </select>
       </div>
 
-      <div id="city">
-        <select onChange={e => setSelectedCity(e.target.value)}>
+      <div>
+        <select id="city" onChange={(e) => setSelectedCity(e.target.value)}>
           <option value="">Select City</option>
-          {cities.map((c, i) => <option key={i} value={c}>{c}</option>)}
+          {cities.map((c, i) => (
+            <option key={i} value={c}>
+              {c}
+            </option>
+          ))}
         </select>
       </div>
 
-      <button id="searchBtn" onClick={handleSearch} type="submit">Search</button>
+      <button id="searchBtn" onClick={handleSearch} type="submit">
+        Search
+      </button>
     </div>
   );
 }
